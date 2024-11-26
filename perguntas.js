343e4186597b1d784e2e3b5e20f8c98c8e3853d9
leolindo07 <0000112986067xsp@al.educacao.sp.gov.br>
2024-11-26 13:57:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geek ',
+    'O que originou os poderes do homem aranha?',
+    'A picada de uma aranha radioativa'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior pais?',
+    'A Russia'
+)
+
+criaCartao(
+    'Geek',
+    'Qual a fraqueza do Superman?',
+    'Kriptonita'
+)
+
+criaCartao(
+    'ingles',
+    'Tomate em Inglês?',
+    'Tomato'
+)
